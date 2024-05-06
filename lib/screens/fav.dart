483c7dcb6014f61230ab895c07ca7dfbc7ebb7fc
zacw-243L <58255472+zacw-243L@users.noreply.github.com>
2024-05-06 10:11:35 +0800

    Update fav.dart

--- lib/screens/fav.dart
+++ lib/screens/fav.dart
@@ -1,1 +1,45 @@
+import 'package:flutter/material.dart';
+import '../models/courses_repository.dart';
+import 'package:provider/provider.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('NYP Courses'),
+        backgroundColor: Colors.blue,
+      ),
+      body: Consumer<CoursesRepository>(
+        builder: (context, courses, child) {
+          return ListView.builder(
+            itemCount: courses.allCoursesCount,
+            itemBuilder: (context, index) {
+              final course = courses.allCourses[index];
+              return ListTile(
+                onTap: () {
+                  Navigator.pushNamed(context, '/detail', arguments: course);
+                },
+                leading: Image.network(course.imageUrl),
+                title: Text(course.title),
+                subtitle: Text(course.code),
+                trailing: IconButton(
+                  icon: (course.isFav
+                      ? const Icon(Icons.favorite, color: Colors.red)
+                      : const Icon(Icons.favorite_border)),
+                  onPressed: () {
+                    courses.updateCourse(course);
+                  },
+                ),
+                tileColor: course.tileColor,
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
 
