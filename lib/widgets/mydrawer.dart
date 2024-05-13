64ca549b01e8f8f2d9c5d12858153daab0d1a8de
zacw-243L <qwerty.amousy123442x@gmail.com>
2024-05-13 18:56:28 +0800

    DONE. DONE. DONE. Finally Fucking Done

--- lib/widgets/mydrawer.dart
+++ lib/widgets/mydrawer.dart
@@ -6,6 +6,7 @@ class MyDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
+      backgroundColor: Colors.black,
       child: SafeArea(
         child: Column(
           children: <Widget>[
@@ -21,22 +22,52 @@ class MyDrawer extends StatelessWidget {
               ),
             ),
             ListTile(
-              title: const Text('Home'),
+              contentPadding: const EdgeInsets.fromLTRB(20.0, 15, 5.0, 15),
+              leading: const Icon(Icons.home, color: Colors.blue),
+              title: const Text(
+                'Home',
+                style: TextStyle(fontSize: 20, color: Colors.white),
+              ),
               onTap: () {
                 Navigator.pushReplacementNamed(context, '/');
               },
+              tileColor: Colors.grey[870],
+            ),
+            Divider(
+              height: 1,
+              color: Colors.grey[300],
             ),
             ListTile(
-              title: const Text('Favourite'),
+              contentPadding: const EdgeInsets.fromLTRB(20.0, 15, 5.0, 15),
+              leading: const Icon(Icons.favorite, color: Colors.red),
+              title: const Text(
+                'Favourite',
+                style: TextStyle(fontSize: 20, color: Colors.white),
+              ),
               onTap: () {
                 Navigator.pushReplacementNamed(context, '/fav');
               },
+              tileColor: Colors.grey[870],
+            ),
+            Divider(
+              height: 1,
+              color: Colors.grey[300],
             ),
             ListTile(
-              title: const Text('Contact Us'),
+              contentPadding: const EdgeInsets.fromLTRB(20.0, 15, 5.0, 15),
+              leading: const Icon(Icons.contact_page, color: Colors.grey),
+              title: const Text(
+                'Contact Us',
+                style: TextStyle(fontSize: 20, color: Colors.white),
+              ),
               onTap: () {
                 Navigator.pushReplacementNamed(context, '/contactus');
               },
+              tileColor: Colors.grey[870],
+            ),
+            Divider(
+              height: 1,
+              color: Colors.grey[300],
             ),
           ],
         ),
