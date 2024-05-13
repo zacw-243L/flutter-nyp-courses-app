bcc6e37c2bffa72ac0e3ab5aaca564767e1099b6
zacw-243L <qwerty.amousy123442x@gmail.com>
2024-05-13 09:49:16 +0800

    Assginment 1 Step 4

--- lib/screens/contactus.dart
+++ lib/screens/contactus.dart
@@ -1,4 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+void launchURL(String url) async {
+  if (await canLaunch(url)) {
+    await launch(url);
+  } else {
+    throw 'Could not launch $url';
+  }
+}
 
 class ContactUsScreen extends StatelessWidget {
   const ContactUsScreen({super.key});
@@ -11,14 +20,14 @@ class ContactUsScreen extends StatelessWidget {
         title: const Text('Contact us'),
         backgroundColor: Colors.blueAccent,
       ),
-      body: const Column(
+      body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          CircleAvatar(
+          const CircleAvatar(
             radius: 50.0,
             backgroundImage: AssetImage('images/image.png'),
           ),
-          Text(
+          const Text(
             'School of Engineering',
             style: TextStyle(
                 fontWeight: FontWeight.bold,
@@ -26,19 +35,20 @@ class ContactUsScreen extends StatelessWidget {
                 color: Colors.cyan,
                 fontFamily: 'Raleway'),
           ),
-          Text('180 Ang Mo Kio Ave 8 Singapore 569830'),
-          SizedBox(height: 20.0),
+          const Text('180 Ang Mo Kio Ave 8 Singapore 569830'),
+          const SizedBox(height: 20.0),
           Padding(
-            padding: EdgeInsets.fromLTRB(5.0, 0, 5.0, 0),
+            padding: const EdgeInsets.fromLTRB(5.0, 0, 5.0, 0),
             child: Card(
               child: ListTile(
-                leading: Icon(Icons.phone, color: Colors.blue),
-                title: Text('6550 0400'),
+                leading: const Icon(Icons.phone, color: Colors.blue),
+                title: const Text('6550 0400'),
+                onTap: () => launchURL('tel:65500400'),
               ),
             ),
           ),
-          SizedBox(height: 2.5),
-          Padding(
+          const SizedBox(height: 2.5),
+          const Padding(
             padding: EdgeInsets.fromLTRB(5.0, 0, 5.0, 0),
             child: Card(
               child: ListTile(
