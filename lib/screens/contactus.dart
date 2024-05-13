362f3e969b11fa1706023cfc4b9053b2e9823f9b
zacw-243L <qwerty.amousy123442x@gmail.com>
2024-05-13 17:46:22 +0800

    Added loction

--- lib/screens/contactus.dart
+++ lib/screens/contactus.dart
@@ -17,6 +17,7 @@ class ContactUsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black87,
       drawer: const MyDrawer(),
       appBar: AppBar(
         title: const Text('Contact us'),
@@ -29,22 +30,28 @@ class ContactUsScreen extends StatelessWidget {
             radius: 50.0,
             backgroundImage: AssetImage('images/image.png'),
           ),
+          const SizedBox(height: 20.0),
           const Text(
             'School of Engineering',
             style: TextStyle(
                 fontWeight: FontWeight.bold,
                 fontSize: 32.0,
-                color: Colors.cyan,
+                color: Colors.lightBlueAccent,
                 fontFamily: 'Raleway'),
           ),
-          const Text('180 Ang Mo Kio Ave 8 Singapore 569830'),
+          const Text(
+            '180 Ang Mo Kio Ave 8 Singapore 569830',
+            style: TextStyle(color: Colors.red),
+          ),
           const SizedBox(height: 20.0),
           Padding(
             padding: const EdgeInsets.fromLTRB(5.0, 0, 5.0, 0),
             child: Card(
+              color: Colors.blue,
               child: ListTile(
-                leading: const Icon(Icons.phone, color: Colors.blue),
-                title: const Text('6550 0400'),
+                leading: const Icon(Icons.phone, color: Colors.white),
+                title: const Text('6550 0400',
+                    style: TextStyle(color: Colors.black)),
                 onTap: () => launchURL('tel:65500400'),
               ),
             ),
@@ -53,13 +60,28 @@ class ContactUsScreen extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.fromLTRB(5.0, 0, 5.0, 0),
             child: Card(
+              color: Colors.blue,
               child: ListTile(
-                leading: const Icon(Icons.email, color: Colors.blue),
-                title: const Text('askNYP@nyp.edu.sg'),
+                leading: const Icon(Icons.email, color: Colors.white),
+                title: const Text('askNYP@nyp.edu.sg',
+                    style: TextStyle(color: Colors.black)),
                 onTap: () => launchURL('mailto:askNYP@nyp.edu.sg'),
               ),
             ),
           ),
+          const SizedBox(height: 2.5),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(5.0, 0, 5.0, 0),
+            child: Card(
+              color: Colors.blue,
+              child: ListTile(
+                leading: const Icon(Icons.place, color: Colors.white),
+                title: const Text('180 Ang Mo Kio Ave 8'),
+                onTap: () => launchURL(
+                    'https://www.google.com/maps/place/Nanyang+Polytechnic/@1.3812389,103.849375,15.25z/data=!3m1!5s0x31da16e96f516fcd:0x733bf8a18a836c59!4m6!3m5!1s0x31da16eb64b0249d:0xe5f10ff680eed942!8m2!3d1.37995!4d103.8489487!16zL20vMDN4Njkz?entry=ttu'),
+              ),
+            ),
+          )
         ],
       ),
     );
