e12e4f7fc3fe31a0f877e4a546d416cf196dddf4
zacw-243L <qwerty.amousy123442x@gmail.com>
2024-05-13 10:08:59 +0800

    Assginment 1 Step 8

--- lib/widgets/mydrawer.dart
+++ lib/widgets/mydrawer.dart
@@ -1,9 +1,40 @@
 import 'package:flutter/material.dart';
+import '../screens/home.dart';
+import '../screens/fav.dart';
+import '../screens/contactus.dart';
 
 class MyDrawer extends StatelessWidget {
   const MyDrawer({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Drawer();
+    return Drawer(
+      child: SafeArea(
+        child: Column(),
+        children: <Widget>[
+          DrawerHeader(
+            decoration: const BoxDecoration(
+              color: Colors.blue,
+            ),
+            child: Container(
+              width: double.infinity,
+              alignment: Alignment.bottomLeft,
+              child: const Text(
+                'NYP',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            ListTile(
+              title: const Text('Home'),
+              onTap: () {
+                Navigator.pushReplacementNamed(context, '/');
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
