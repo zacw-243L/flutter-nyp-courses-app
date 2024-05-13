019ac5263a1011fb2a1438fb2ed460a81a73c01d
zacw-243L <qwerty.amousy123442x@gmail.com>
2024-05-13 18:11:14 +0800

    Finished adding developer Details

--- lib/screens/contactus.dart
+++ lib/screens/contactus.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 import '../widgets/mydrawer.dart';
+import 'package:footer/footer.dart';
+import 'package:footer/footer_view.dart';
 
-void launchURL(String url) async {
+Future<void> launchURL(String url) async {
   if (await canLaunch(url)) {
     await launch(url);
   } else {
@@ -13,7 +15,6 @@ void launchURL(String url) async {
 class ContactUsScreen extends StatelessWidget {
   const ContactUsScreen({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,65 +24,79 @@ class ContactUsScreen extends StatelessWidget {
         title: const Text('Contact us'),
         backgroundColor: Colors.blueAccent,
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+      body: FooterView(
+        footer: Footer(
+          backgroundColor: Colors.grey,
+          padding: const EdgeInsets.fromLTRB(2, 0, 2, 0),
+          child: const Center(child: Text('Made By: Ong Zachary (221252K)')),
+        ),
+        flex: 2,
         children: [
-          const CircleAvatar(
-            radius: 50.0,
-            backgroundImage: AssetImage('images/image.png'),
-          ),
-          const SizedBox(height: 20.0),
-          const Text(
-            'School of Engineering',
-            style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontSize: 32.0,
-                color: Colors.lightBlueAccent,
-                fontFamily: 'Raleway'),
-          ),
-          const Text(
-            '180 Ang Mo Kio Ave 8 Singapore 569830',
-            style: TextStyle(color: Colors.red),
-          ),
-          const SizedBox(height: 20.0),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(5.0, 0, 5.0, 0),
-            child: Card(
-              color: Colors.blue,
-              child: ListTile(
-                leading: const Icon(Icons.phone, color: Colors.white),
-                title: const Text('6550 0400',
-                    style: TextStyle(color: Colors.black)),
-                onTap: () => launchURL('tel:65500400'),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(height: 20.0),
+              const SizedBox(height: 20.0),
+              const SizedBox(height: 20.0),
+              const SizedBox(height: 20.0),
+              const CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/image.png'),
               ),
-            ),
-          ),
-          const SizedBox(height: 2.5),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(5.0, 0, 5.0, 0),
-            child: Card(
-              color: Colors.blue,
-              child: ListTile(
-                leading: const Icon(Icons.email, color: Colors.white),
-                title: const Text('askNYP@nyp.edu.sg',
-                    style: TextStyle(color: Colors.black)),
-                onTap: () => launchURL('mailto:askNYP@nyp.edu.sg'),
+              const SizedBox(height: 20.0),
+              const Text(
+                'School of Engineering',
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 32.0,
+                    color: Colors.lightBlueAccent,
+                    fontFamily: 'Raleway'),
               ),
-            ),
-          ),
-          const SizedBox(height: 2.5),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(5.0, 0, 5.0, 0),
-            child: Card(
-              color: Colors.blue,
-              child: ListTile(
-                leading: const Icon(Icons.place, color: Colors.white),
-                title: const Text('180 Ang Mo Kio Ave 8'),
-                onTap: () => launchURL(
-                    'https://www.google.com/maps/place/Nanyang+Polytechnic/@1.3812389,103.849375,15.25z/data=!3m1!5s0x31da16e96f516fcd:0x733bf8a18a836c59!4m6!3m5!1s0x31da16eb64b0249d:0xe5f10ff680eed942!8m2!3d1.37995!4d103.8489487!16zL20vMDN4Njkz?entry=ttu'),
+              const Text(
+                '180 Ang Mo Kio Ave 8 Singapore 569830',
+                style: TextStyle(color: Colors.red),
               ),
-            ),
-          )
+              const SizedBox(height: 20.0),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(5.0, 0, 5.0, 0),
+                child: Card(
+                  color: Colors.blue,
+                  child: ListTile(
+                    leading: const Icon(Icons.phone, color: Colors.white),
+                    title: const Text('6550 0400',
+                        style: TextStyle(color: Colors.black)),
+                    onTap: () => launchURL('tel:65500400'),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 2.5),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(5.0, 0, 5.0, 0),
+                child: Card(
+                  color: Colors.blue,
+                  child: ListTile(
+                    leading: const Icon(Icons.email, color: Colors.white),
+                    title: const Text('askNYP@nyp.edu.sg',
+                        style: TextStyle(color: Colors.black)),
+                    onTap: () => launchURL('mailto:askNYP@nyp.edu.sg'),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 2.5),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(5.0, 0, 5.0, 0),
+                child: Card(
+                  color: Colors.blue,
+                  child: ListTile(
+                    leading: const Icon(Icons.place, color: Colors.white),
+                    title: const Text('180 Ang Mo Kio Ave 8'),
+                    onTap: () => launchURL(
+                        'https://www.google.com/maps/place/Nanyang+Polytechnic/@1.3812389,103.849375,15.25z/data=!3m1!5s0x31da16e96f516fcd:0x733bf8a18a836c59!4m6!3m5!1s0x31da16eb64b0249d:0xe5f10ff680eed942!8m2!3d1.37995!4d103.8489487!16zL20vMDN4Njkz?entry=ttu'),
+                  ),
+                ),
+              )
+            ],
+          ),
         ],
       ),
     );
